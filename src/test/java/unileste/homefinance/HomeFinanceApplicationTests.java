6bb51b19dd6d7b0233bfe5f8b1870254e0355d12
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2026-03-19 20:02:42 -0300

    chore/HomeFinance: Criando o projeto

---
+++ src/test/java/unileste/homefinance/HomeFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package unileste.homefinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

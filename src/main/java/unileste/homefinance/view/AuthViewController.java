f81eafed7b7fba5555c1f1ec0998ad50a95087a6
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2026-03-26 15:18:16 -0300

    feat/emailConfirmedView: criando View para link de redirecionamento apos confirmação de email

---
+++ src/main/java/unileste/homefinance/view/AuthViewController.java
@@ -0,0 +1,13 @@
+package unileste.homefinance.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthViewController {
+
+    @GetMapping("/public/email-confirmed")
+    public String emailConfirmedPage() {
+        return "email-confirmed"; // nome do arquivo SEM .html
+    }
+}
